9d79aa4f882aad8b036e8f10222f58ec58fe1f18
Daniel Lagos <daniel.lagos010@gmail.com>
2021-01-13 21:17:14 -0500

    Create auth components

---
+++ components/auth/AuthScreen.js
@@ -0,0 +1,13 @@
+import Styles from './AuthScreen.css';
+
+const AuthScreen = () => {
+    return (<div className={Styles.screen}>
+        <div className={Styles.auth}>
+
+
+
+        </div>
+    </div>);
+}
+
+export default AuthScreen;
