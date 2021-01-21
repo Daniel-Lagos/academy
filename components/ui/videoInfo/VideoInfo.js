0495d031e4aee9f3fb02ae4bb8788e2bea6faf1c
Arge Niño <arge.nino@uptc.edu.co>
2021-01-21 13:05:10 -0500

    modify videoInfo and module.css

--- components/ui/videoInfo/VideoInfo.js
+++ components/ui/videoInfo/VideoInfo.js
@@ -3,21 +3,21 @@ import Image from 'next/image';
 
 import Styles from './VideoInfo.module.css';
 
-export default function VideoInfo(){
+export default function VideoInfo({views,likes}) {
     return (
-      <>
-          <div className={Styles.main}>
+        <>
+            <div className={Styles.main}>
                 <div className='row'>
-                    <div className={Styles.cont}>
-                        <div className='col-4'>
-                            <Image className={Styles.image} src='/profile.jpg' width={800} height={550}/>
-                        </div>
+                    <div className={`${Styles.cont} col-sm-4`}>
+                        <Image className={Styles.image} src='/profile.jpg' width={250} height={150}/>
                     </div>
-                    <div className='col-8'>
-                        Contenedor 2
+                    <div className={`${Styles.contTwo} col-sm-8`}>
+                        <h4>Titulo del video segun la categoria desde la API de Youtube 🚀</h4>
+                        <p>👁‍🗨 {views} views  👍🏻 {likes} likes</p>
+                        
                     </div>
                 </div>
-          </div>
-      </>
+            </div>
+        </>
     );
 }
