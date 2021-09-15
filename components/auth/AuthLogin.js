bf5ffe054b237db4fac1ad9422ca1f12aa7d73e2
Daniel Lagos <daniel.lagos010@gmail.com>
2021-09-15 12:21:42 -0500

    set new values in input fileds

--- components/auth/AuthLogin.js
+++ components/auth/AuthLogin.js
@@ -1,20 +1,23 @@
 import Styles from './AuthScreen.module.css';
-import { useContext, useState } from 'react';
+import { useContext } from 'react';
 import { SessionContext } from '../../providers/sessionContext';
 import fetch from 'isomorphic-fetch';
+import { useForm } from '../../hooks/useForm';
 
 const AuthLogin = () => {
   const { session, setSession } = useContext(SessionContext);
-  const [password, setPassword] = useState('');
-  const [email, setEmail] = useState('');
+
+  const [formLoginValues, handleLoginInputChange] = useForm({
+    email: '',
+    password: ''
+  });
+
+  console.log(formLoginValues.email);
+  console.log(formLoginValues.password);
 
   const handleSubmit = async (e) => {
     e.preventDefault();
-    const response = await fetch('',{});
-  };
-
-  const handleInputEmail = () => {
-
+    const response = await fetch('', {});
   };
 
   return (
@@ -23,12 +26,16 @@ const AuthLogin = () => {
         name={'email'}
         placeholder="Email"
         type="email"
+        value={formLoginValues.email}
+        onChange={handleLoginInputChange}
       />
       <br/>
       <input
         name={'password'}
         placeholder="Password"
         type="password"
+        value={formLoginValues.password}
+        onChange={handleLoginInputChange}
       />
       <p className={Styles.passwordText}><a href="www.google.com"
                                             style={{ color: 'black' }}> Forgot
