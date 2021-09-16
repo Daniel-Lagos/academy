8fac93c38526d62336c1d0519539be67169b0cf7
Arge Niño <arge.nino@uptc.edu.co>
2021-09-16 16:01:01 -0500

    POST requests made in AuthLogin and AuthRegister

--- components/auth/AuthLogin.js
+++ components/auth/AuthLogin.js
@@ -1,57 +1,74 @@
 import Styles from './AuthScreen.module.css';
-import { useContext } from 'react';
-import { SessionContext } from '../../providers/sessionContext';
-import { useForm } from '../../hooks/useForm';
+import {useContext} from 'react';
+import {SessionContext} from '../../providers/sessionContext';
+import {useForm} from '../../hooks/useForm';
 
 const AuthLogin = () => {
-  const { setSession } = useContext(SessionContext);
+    const {setSession} = useContext(SessionContext);
 
-  const [formLoginValues, handleLoginInputChange] = useForm({
-    email: '',
-    password: ''
-  });
+    const [formLoginValues, handleLoginInputChange] = useForm({
+        email: '',
+        password: ''
+    });
 
-  const handleSubmit = async (e) => {
-    e.preventDefault();
-    // const response = await fetch('', {});
+    const handleSubmit = async (e) => {
+        e.preventDefault();
+        fetch('https://backend-academy.herokuapp.com/api/auth', {
+            method: 'POST',
+            headers: {
+                'Content-Type': 'application/json'
+            },
+            body: JSON.stringify({
+                password: formLoginValues.password,
+                email: formLoginValues.email
+            })
+        }).then(response => response.json())
+            .then(data => {
+                if (data.success) {
+                    console.log('Ingreso exitoso');
+                    setSession(
+                        {
+                            email: formLoginValues.email,
+                            password: formLoginValues.password
+                        });
+                } else {
+                    console.log(data.message);
+                }
+            })
 
-    setSession(
-      {
-        email: formLoginValues.email,
-        password: formLoginValues.password
-      });
-  };
 
-  return (
-    <form className={Styles.inputLogin}>
-      <input
-        name={'email'}
-        placeholder="Email"
-        type="email"
-        value={formLoginValues.email}
-        onChange={handleLoginInputChange}
-      />
-      <br/>
-      <input
-        name={'password'}
-        placeholder="Password"
-        type="password"
-        value={formLoginValues.password}
-        onChange={handleLoginInputChange}
-      />
-      <p className={Styles.passwordText}><a href="www.google.com"
-                                            style={{ color: 'black' }}> Forgot
-        Password?</a></p>
+    };
 
-      <button
-        className={Styles.buttonLogin}
-        type="onSubmit"
-        onClick={handleSubmit}
-      >
-        <span>Log in</span>
-      </button>
-    </form>
-  );
+    return (
+        <form className={Styles.inputLogin}>
+            <input
+                name={'email'}
+                placeholder="Email"
+                type="email"
+                value={formLoginValues.email}
+                onChange={handleLoginInputChange}
+            />
+            <br/>
+            <input
+                name={'password'}
+                placeholder="Password"
+                type="password"
+                value={formLoginValues.password}
+                onChange={handleLoginInputChange}
+            />
+            <p className={Styles.passwordText}><a href="www.google.com"
+                                                  style={{color: 'black'}}> Forgot
+                Password?</a></p>
+
+            <button
+                className={Styles.buttonLogin}
+                type="onSubmit"
+                onClick={handleSubmit}
+            >
+                <span>Log in</span>
+            </button>
+        </form>
+    );
 };
 
 export default AuthLogin;
--- components/auth/AuthRegister.js
+++ components/auth/AuthRegister.js
@@ -1,33 +1,89 @@
 import Styles from './AuthScreen.module.css';
+import {useContext} from 'react';
+import {SessionContext} from '../../providers/sessionContext';
+import {useForm} from '../../hooks/useForm';
 
 const AuthRegister = () => {
-    const handleSubmit = (e) => {
+    const {setSession} = useContext(SessionContext);
+
+    const [formRegisterValues, handRegisterInputChangue] = useForm({
+        name: '',
+        surname: '',
+        password: '',
+        email: '',
+        role: ''
+    });
+
+    const handleSubmit = async (e) => {
         e.preventDefault();
-        console.log('Entro')
+
+        fetch('https://backend-academy.herokuapp.com/api/auth/new',
+            {
+                method: 'POST',
+                headers: {
+                    'Content-Type': 'application/json'
+                },
+                body: JSON.stringify({
+                    name: formRegisterValues.name,
+                    surname: formRegisterValues.surname,
+                    password: formRegisterValues.password,
+                    email: formRegisterValues.email,
+                    role: formRegisterValues.role
+                })
+            }).then(response => response.json())
+            .then(data => {
+                if (data.success) {
+                    console.log('Registro exitoso')
+                    setSession({
+                        name: formRegisterValues.name,
+                        surname: formRegisterValues.surname,
+                        password: formRegisterValues.password,
+                        email: formRegisterValues.email,
+                        role: 'User_role'
+                    });
+                } else {
+                    console.log(data.message);
+                }
+            })
     }
     return (
         <form className={Styles.inputLogin}>
             <input
+                name={'name'}
                 placeholder="Name"
                 type="text"
+                value={formRegisterValues.name}
+                onChange={handRegisterInputChangue}
             />
             <br/>
             <input
+                name={'surname'}
                 placeholder="Surname"
                 type="text"
+                value={formRegisterValues.surname}
+                onChange={handRegisterInputChangue}
             />
             <br/>
             <input
+                name={'password'}
                 placeholder="Password"
                 type="password"
+                value={formRegisterValues.password}
+                onChange={handRegisterInputChangue}
             />
             <input
+                name={'email'}
                 placeholder="Email"
                 type="email"
+                value={formRegisterValues.email}
+                onChange={handRegisterInputChangue}
             />
             <input
+                name={'role'}
                 placeholder="Role"
                 type="text"
+                value={formRegisterValues.role}
+                onChange={handRegisterInputChangue}
             />
             <button
                 className={Styles.buttonLogin}
